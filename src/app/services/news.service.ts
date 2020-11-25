aa8d9cabae38ce014efa2718acd8751fd7bb08c0
r2magarcia <ma.garcia2r@gmail.com>
2020-11-25 17:22:59 -0500

    creo que ahora si

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -7,13 +7,11 @@ interface NewsI {
   header: {
     id: number
     title: string
-    icon?: string
     categories: Array<string>
   }
   body: {
     img: string
     content: string
-    preview: string
     imgSize?: number
     contentSize?: number
   }
@@ -24,6 +22,7 @@ interface SectionsI {
   id: number,
   title: string,
   news: Array<NewsI>
+  icon?: string
 }
 
 @Injectable({
@@ -43,19 +42,19 @@ export class NewsService {
     {
       id: 1, 
       title: "Noticias Academicas", 
+      icon: "medkit-outline",
       news: [
       {
         header: {
           id: 1,
           title: "Lorep ipsum",
-          icon: "medkit-outline",
           categories: ["Covid-19", "salud"]
         },
 
         body: {
-          img: "https://picsum.photos/500/300?random=1",
+          img: "https://picsum.photos/500/400?random=1",
           content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-          preview:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...",
+          // preview:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...",
 
         }
       },
@@ -64,14 +63,13 @@ export class NewsService {
         header: {
           id: 2,
           title: "otra noticia academica",
-          icon: "newspaper-outline",
           categories: ["Noticia", "General"]
         },
 
         body: {
-          img: "https://picsum.photos/500/300?random=2",
+          img: "https://picsum.photos/500/400?random=2",
           content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-          preview:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...",
+          // preview:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...",
         }
       }
     ]},
@@ -79,19 +77,19 @@ export class NewsService {
     {
       id: 2, 
       title: "Noticias Generales", 
+      icon: "bag-handle-outline",
       news: [
         {
           header: {
             id: 3,
             title: "Otro titulo de noticia",
-            icon: "bag-handle-outline",
             categories: ["Ciudad", "Nose"]
           },
 
           body: {
-            img: "https://picsum.photos/500/300?random=3",
+            img: "https://picsum.photos/500/400?random=3",
             content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-            preview:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...",
+            // preview:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...",
           }
         }
       ]
@@ -125,13 +123,16 @@ export class NewsService {
   getNewsByName(search: string): Array<SectionsI>{
     let sec: Array<SectionsI> = [];
     let newww: Array<NewsI> = [];
+    let newispushed:boolean=false;
     //el.name.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").indexOf(name.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "")) > -1;
     this.sections.forEach(element => {
       newww = [];
       element.news.forEach(neww => {
+        newispushed=false;
         neww.header.categories.map(el => {
-          if(el.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").indexOf(search.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "")) > -1){
+          if(el.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").indexOf(search.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "")) > -1 && !newispushed){
             newww.push(neww);
+            newispushed=true;
           };
         })
       });
